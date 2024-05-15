df04ca3e1a2561be7b55d7ca9e00e786104ea237
Gabriel Tegani <gabrielbtegani@gmail.com>
2024-05-14 22:24:25 -0300

    Adding typescript

---
+++ src/components/Comment.tsx
@@ -0,0 +1,58 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar.tsx'
+import { useState } from 'react'
+
+interface CommentProps {
+   content: string
+   onDeleteComment: (comment: string) => void
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+   const [likeCount, setLikeCount] = useState(0)
+
+   const handleLikeComment = () => {
+      setLikeCount((state) => {
+         return state + 1
+      })
+   }
+
+   const handleDeleteComment = () => {
+      onDeleteComment(content);
+   }
+
+   return (
+      <div className={styles.comment}>
+         <Avatar hasBorder={false} src="https://github.com/GLtegani.png" alt='' />
+
+         <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+               <header>
+                  <div className={styles.authorAndTime}>
+                     <strong>Gabriel Tegani</strong>
+
+                     <time title='11 de Maio às 08:13' dateTime="2023-05-11 08:13:56">
+                        Cerca de 1h atrás
+                     </time>
+                  </div>
+
+                  <button onClick={handleDeleteComment} title='Deletar comentário'>
+                     <Trash size={24} />
+                  </button>
+               </header>
+
+               <p>
+                  {content}
+               </p>
+            </div>
+
+            <footer>
+               <button onClick={handleLikeComment}>
+                  <ThumbsUp />
+                  Aplaudir <span>{likeCount}</span>
+               </button>
+            </footer>
+         </div>
+      </div>
+   )
+}
